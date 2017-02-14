bfa3976d9fd4dbd3e3add89e17e9cf95f5a2384f
ololook <lfskyx@qq.com>
2017-02-14 09:30:31 +0800

    修复bug
    适用于数据迁移过后，进行数据比对

--- checksum.py
+++ checksum.py
@@ -1,4 +1,3 @@
-#encoding: utf-8
 from multiprocessing import Pool
 import time
 import sys
@@ -8,13 +7,15 @@ import cx_Oracle
 import hashlib
 from sys import argv
 reload(sys )
-sys.setdefaultencoding('utf8') 
+sys.setdefaultencoding('utf8')
+error=[]
+presult=[]
 def source_client():
       try:
          con = MySQLdb.connect(host ='127.0.0.1',
-                             user ='usrname',
+                             user ='username',
                              passwd ='passwd',
-                             port =port,
+                             port =7307,
                              charset='utf8',
                              cursorclass = MySQLdb.cursors.SSCursor
                               )
@@ -24,7 +25,7 @@ def source_client():
  
 def source_table():
        cur = source_client().cursor()
-       sql_1="select * from gmonitor.backupset where 0=1"
+       sql_1="select * from gmonitor.test_01 where 0=1"
        row=""
        try:
           cur.execute(sql_1)
@@ -36,16 +37,16 @@ def source_table():
            else:
                   row +=','+cur.description[i][0]
                   i=i+1
-           cols="select "+"concat_ws"+"("+'"||"'+","+row+")"+" "+"from "+"gmonitor.backupset"
+           cols="select "+"concat_ws"+"("+'"||"'+","+row+")"+" "+"from "+"gmonitor.test_01 order by id "
        return cols
 
 
 def destin_client():
       try:
          con = MySQLdb.connect(host ='127.0.0.1',
-                             user ='usrname',
+                             user ='username',
                              passwd ='passwd',
-                             port =port,
+                             port =7307,
                              charset='utf8',
                              cursorclass = MySQLdb.cursors.SSCursor
                               )
@@ -56,7 +57,7 @@ def destin_client():
 
 def destin_table():
        cur = destin_client().cursor()
-       sql_1="select * from gmonitor.backupset_bak where 0=1"
+       sql_1="select * from gmonitor.test_02 where 0=1"
        row=""
        try:
           cur.execute(sql_1)
@@ -68,29 +69,46 @@ def destin_table():
            else:
                   row +=','+cur.description[i][0]
                   i=i+1
-           cols="select "+"concat_ws"+"("+'"||"'+","+row+")"+" "+"from "+"gmonitor.backupset_bak"
+           cols="select "+"concat_ws"+"("+'"||"'+","+row+")"+" "+"from "+"gmonitor.test_02 order by id"
        return cols
 
+def remove_values_from_list(the_list, val):
+    return [value for value in the_list if value != val]
 
 def compare_row(source_1,destin_1,num):
-    
     offset=num
     lg=len(source_1)
     for i in range(0,lg,offset):
-        source1_md5=hashlib.md5(str(source_1[i:i+offset])).hexdigest()
-        destin1_md5=hashlib.md5(str(destin_1[i:i+offset])).hexdigest()
-        if offset==1 and source1_md5!=destin1_md5:
-           print source_1[i:i+offset]
-           print destin_1[i:i+offset]
-           print 
-         
-        elif source1_md5!=destin1_md5:
-            compare_row(source_1,destin_1,1)        
-        else:
-           pass
-
+        destin2=destin_1[i:i+offset]
+        source2=source_1[i:i+offset]
+        source1_md5=hashlib.md5(str(source2)).hexdigest()
+        destin1_md5=hashlib.md5(str(destin2)).hexdigest()
+        if  source1_md5 != destin1_md5:
+           for s in source2:
+               s_md=hashlib.md5(str(s)).hexdigest()
+               for d in destin2:
+                   d_md=hashlib.md5(str(d)).hexdigest()
+                   if s_md==d_md:
+                      while s in error: 
+                            error.remove(s)
+                      break
+                   else:
+                      if s not in error:
+                         error.append(s)
+        else:   
+           continue
+    for x in error:
+        x_md=hashlib.md5(str(x)).hexdigest()
+        for y in destin_1:
+            y_md=hashlib.md5(str(y)).hexdigest()
+            if x_md==y_md:
+               while x in error: 
+                    error.remove(x)
+               break
+            else:
+               pass
 def compare_table():
-    batch=25000
+    batch=500
     lag=0
     source_cur=source_client().cursor()
     destin_cur=destin_client().cursor()
@@ -99,18 +117,18 @@ def compare_table():
     source_cur.execute(source_sql)
     destin_cur.execute(destin_sql)
     source_result = source_cur.fetchmany(batch) 
-    while source_result:
-          lag=lag+1
+    while source_result: 
           destin_result = destin_cur.fetchmany(batch)
           source_md5=hashlib.md5(str(source_result)).hexdigest()
           destin_md5=hashlib.md5(str(destin_result)).hexdigest()
           if source_md5 !=destin_md5:
-             compare_row(source_result,destin_result,500)
+             compare_row(source_result,destin_result,100) 
           else:
              pass
           source_result=[]
           destin_result=[]
           source_result = source_cur.fetchmany(batch)
-
+    for i in error:
+        print i
 if __name__ == '__main__':
     compare_table()
